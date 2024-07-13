b14ecc2210e04e242ca65e9c0f0ef2bfcae2eb36
Anthos Koutouris <kountourisa@cardiff.ac.uk>
2024-07-13 14:01:09 +0100

    Adds theme to the templates

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms # adding this to customize the form
 from .models import Project
 
 
@@ -7,3 +8,19 @@ class ProjectForm(ModelForm):
         model = Project # name of model
         # fields = '__all__' # fields from the model to add on the form
         fields = ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tags']
+        widgets = {
+            'tags':forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        # self.fields['title'].widget.attrs.update({'class' : 'input', 'placeholder': 'Add title' })
+
+        for name,field in self.fields.items():
+            field.widget.attrs.update({'class' : 'input'})
+        # self.fields['description'].widget.attrs.update({'class' : 'input', 'placeholder': 'Add description' })
+
+        # self.fields['demo_link'].widget.attrs.update({'class' : 'input', 'placeholder': 'Add demo link' })
+
+        # self.fields['demo_link'].widget.attrs.update({'class' : 'input', 'placeholder': 'Add demo link' })
