d5a55f51b4f7b68b32eaebf69cb748f0c38531c1
Anthos Koutouris <kountourisa@cardiff.ac.uk>
2024-08-03 13:33:54 +0100

    Add User Account

--- users/views.py
+++ users/views.py
@@ -78,5 +78,15 @@ def userProfile(request, pk):
 
     context = {'profile' : profile,
                "topSkills" : topSkills,
-               "otherSkills": otherSkills}
+               "otherSkills" : otherSkills}
     return render(request, 'users/user-profile.html', context)
+
+@login_required(login_url='login')
+def userAccount(request):
+    profile = request.user.profile # Getting logged in users profile
+
+    skills = profile.skill_set.all()
+    projects = profile.project_set.all()
+
+    context = {'profile' : profile, 'skills' : skills, 'projects' : projects}
+    return render(request, 'users/account.html', context)
