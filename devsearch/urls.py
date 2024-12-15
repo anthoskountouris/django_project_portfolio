fc01de933eec1d4b7ad00a57c9bbb67d2b99736f
Anthos Koutouris <kountourisa@cardiff.ac.uk>
2024-12-15 18:04:51 +0000

    Password Reset

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -20,10 +20,26 @@ from django.urls import path, include
 from django.conf import settings # we want access to our setting file to find the media root and url
 from django.conf.urls.static import static # It's going to heko us create a url for the static file
 
+from django.contrib.auth import views as auth_views # password reset
+
 urlpatterns = [
     path('admin/', admin.site.urls), # We cannot access it until we have the database ready
     path('projects/', include('projects.urls')),
-    path('', include('users.urls'))
+    path('', include('users.urls')),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="password_reset.html"),
+         name = "password_reset"), # user submits emai for reset
+
+    path('reset_pasword_sent/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"),
+         name = "password_reset_done"), # Email sent message
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="reset.html"),
+         name = "password_reset_confirm"), # Email with link and reset instructions
+
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"),
+         name = "password_reset_complete"), # Password succesfully reset message
+
+    
 ]
 
 
