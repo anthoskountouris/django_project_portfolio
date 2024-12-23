c82b2d32114fabc5b3dec9fa17e9d7fe50703c22
Anthos Koutouris <kountourisa@cardiff.ac.uk>
2024-12-23 12:21:21 +0200

    Sending POST Requests

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
-from projects.models import Project
+from projects.models import Project, Review
 
 # All the routes that we're gonna have in our api
 '''Json web tokens have an expiration date so when a user's token expires
@@ -33,5 +33,25 @@ def getProjects(request):
 # @permission_classes([IsAuthenticated])
 def getProject(request, pk):
     project = Project.objects.get(id = pk)
+    serializer = ProjectSerializer(project, many=False) # This turns our python data and converts it into json data
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def projectVote(request, pk):
+    project = Project.objects.get(id = pk)
+    user = request.user.profile # This user is coming from the token not the session becuase of the decorator (api_view)
+    data = request.data # Gives us access to the request (post) data
+    
+    review, created = Review.objects.get_or_create(
+        owner = user,
+        project = project
+    )
+
+    review.value = data['value']
+    review.save()
+    project.getVoteCount
+
+
+
     serializer = ProjectSerializer(project, many=False) # This turns our python data and converts it into json data
     return Response(serializer.data)
